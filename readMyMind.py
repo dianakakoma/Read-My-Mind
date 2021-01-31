ce1bdd02487a1f19555c0b011ea7a7e18f26b175
dianakakoma <dikakoma@gmail.com>
2021-01-31 08:04:39 -0600

    wrote out the game plan = in psuedo-code

---
+++ readMyMind.py
@@ -0,0 +1,8 @@
+#Build a game that asks the user to think of a number and then gets the compute to get that number. 
+#import the random module
+#right function that accepts the high limit for guesses as a parameter
+#declare the variables
+#ask the user for number within a certain range
+#ask the computer to guess the users
+#tell the computer if their guess is too high, too low or correct and then adjust the range of possibilities accordingly.
+#call the function
